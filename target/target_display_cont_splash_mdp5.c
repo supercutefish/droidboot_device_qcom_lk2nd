9ba1c3285f4fdae448b5dc8ec7762ce819400aeb
luka177 <lukapanio@gmail.com>
2022-10-12 19:07:11 +0300

    Add DMA pipes to fix display on 8226

--- target/target_display_cont_splash_mdp5.c
+++ target/target_display_cont_splash_mdp5.c
@@ -25,6 +25,30 @@ static struct msm_panel_info dummy_panel = {
 	.pipe_type = MDSS_MDP_PIPE_TYPE_RGB,
 };
 
+struct pipe {
+	unsigned int base;
+	uint32_t type;
+};
+
+/*
+ * VIG/DMA pipes are weird, but some devices use that...
+ * Might need more changes to make them work properly.
+ */
+static const struct pipe pipes[] = {
+	{
+		.base = MDP_VP_0_RGB_0_BASE,
+		.type = MDSS_MDP_PIPE_TYPE_RGB,
+	},
+	{
+		.base = MDP_VP_0_VIG_0_BASE,
+		.type = MDSS_MDP_PIPE_TYPE_VIG,
+	},
+	{
+		.base = MDP_VP_0_DMA_0_BASE,
+		.type = MDSS_MDP_PIPE_TYPE_DMA,
+	},
+};
+
 extern int check_aboot_addr_range_overlap(uintptr_t start, uint32_t size);
 extern int check_ddr_addr_range_bound(uintptr_t start, uint32_t size);
 
@@ -34,17 +58,22 @@ static int mdp5_read_config(struct fbcon_config *fb)
 	uint32_t stride, src_size, out_size, src_xy, out_xy, size;
 	bool cmd_mode;
 
-	fb->base = (void*) readl(MDP_PIPE_BASE + PIPE_SSPP_SRC0_ADDR);
-	if (!fb->base) {
-		dprintf(CRITICAL, "Continuous splash does not appear to be enabled\n");
+    const struct pipe *pipe = pipes, *pipe_end = pipe + ARRAY_SIZE(pipes);
+    for (; pipe < pipe_end; pipe++) {
+		fb->base = (void*) readl(pipe->base + PIPE_SSPP_SRC0_ADDR);
+		if (fb->base)
+			break;
+	}
+	if (pipe == pipe_end) {
+		dprintf(CRITICAL, "No continuous splash: cannot find active pipe\n");
 		return -1;
 	}
 
-	stride = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE);
-	src_size = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_IMG_SIZE);
-	out_size = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_OUT_SIZE);
-	src_xy = readl(MDP_PIPE_BASE + PIPE_SSPP_SRC_XY);
-	out_xy = readl(MDP_PIPE_BASE + PIPE_SSPP_OUT_XY);
+    stride = readl(pipe->base + PIPE_SSPP_SRC_YSTRIDE);
+	src_size = readl(pipe->base + PIPE_SSPP_SRC_IMG_SIZE);
+	out_size = readl(pipe->base + PIPE_SSPP_SRC_OUT_SIZE);
+	src_xy = readl(pipe->base + PIPE_SSPP_SRC_XY);
+	out_xy = readl(pipe->base + PIPE_SSPP_OUT_XY);
 	cmd_mode = !!(readl(MDP_CTL_BASE + CTL_TOP) & MDP_CTL_TOP_MODE_SEL_CMD);
 
 	dprintf(SPEW, "Continuous splash detected: base: %p, stride: %d, "
