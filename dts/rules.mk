8a4334e3b454696d3792f5f90bfbc0628b3c0378
supercutefish <137264552+supercutefish@users.noreply.github.com>
2023-09-22 13:31:21 +0800

    Add files via upload

--- dts/rules.mk
+++ dts/rules.mk
@@ -30,7 +30,8 @@ endif
 ifeq ($(PROJECT), msm8937-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8937-nokia-nd1.dtb \
-	$(LOCAL_DIR)/msm8937-motorola-cedric.dtb
+	$(LOCAL_DIR)/msm8937-motorola-cedric.dtb \
+	$(LOCAL_DIR)/msm8937-xtc_z7.dtb 
 endif
 ifeq ($(PROJECT), msm8226-secondary)
 DTBS += \
